5a466bc61b8d87cadb219492852acb960ca88f63
yuchenguang <546448821@qq.com>
2020-12-10 21:50:16 +0800

    又一晚

--- controller/seo.js
+++ controller/seo.js
@@ -1,7 +1,7 @@
 const { exec} = require('../db/mysql');
 
 const getSeo = () => {
-  let sql = `SELECT page.pageDescription, page.pageKey, page.pageName from page UNION ALL SELECT category.pageDescription,category.pageKey,category.pageName FROM category`;
+  let sql = `SELECT page.id,page.pageDescription, page.pageKey, page.pageName from page UNION ALL SELECT category.pageId,category.pageDescription,category.pageKey,category.pageName FROM category`;
   return exec(sql);
 }
 module.exports ={
